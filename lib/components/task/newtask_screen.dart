f3b2bce302d3ae8e9c560eff5c6d7cfc6a9964e6
mischiefluis <loidatolentino358@gmail.com>
2025-05-25 00:14:39 +0800

    create task update

--- lib/components/task/newtask_screen.dart
+++ lib/components/task/newtask_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:taskify/utils/colors.dart';
 
 class NewTaskScreen extends StatefulWidget {
@@ -9,9 +10,20 @@ class NewTaskScreen extends StatefulWidget {
 }
 
 class _NewTaskScreenState extends State<NewTaskScreen> {
+  String _category = "School";
+
+  void changeCategory(String? selectedCategory) {
+    if (selectedCategory is String) {
+      setState(() {
+        _category = selectedCategory;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         foregroundColor: AppColors.white,
         title: Text(
@@ -34,7 +46,7 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
             SizedBox.expand(
               child: Container(
                 padding: EdgeInsets.only(
-                  top: MediaQuery.of(context).padding.top + kToolbarHeight + 30,
+                  top: MediaQuery.of(context).padding.top + kToolbarHeight + 10,
                   left: 16,
                   right: 16,
                 ),
@@ -80,6 +92,80 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
                         ),
                       ),
                     ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Description",
+                      style: TextStyle(
+                        color: AppColors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    TextField(
+                      cursorColor: AppColors.white,
+                      style: TextStyle(
+                        color: AppColors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Add a description",
+                        hintStyle: TextStyle(color: AppColors.white),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: AppColors.white,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: AppColors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Category",
+                      style: TextStyle(
+                        color: AppColors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        border: Border.all(width: 2, color: AppColors.white),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                          value: _category,
+                          iconEnabledColor: AppColors.white,
+                          dropdownColor: AppColors.color1,
+                          isExpanded: true,
+                          style: TextStyle(
+                            color: AppColors.white,
+                            fontFamily: GoogleFonts.montserrat().fontFamily,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          items: const [
+                            DropdownMenuItem(
+                              value: "School",
+                              child: Text("School"),
+                            ),
+                            DropdownMenuItem(
+                              value: "Daily",
+                              child: Text("Daily"),
+                            ),
+                          ],
+                          onChanged: changeCategory,
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
@@ -90,13 +176,32 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
               left: 0,
               right: 0,
               child: Container(
-                height: 400,
+                height: 380,
+                padding: EdgeInsets.all(16),
                 decoration: BoxDecoration(
                   color: AppColors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(8),
                     topRight: Radius.circular(8),
                   ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Time Preference",
+                      style: TextStyle(
+                        color: AppColors.colorText,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        // * TODO!
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -105,23 +210,4 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
       ),
     );
   }
-}
-
-DraggableScrollableSheet _buildDraggableScrollableSheet() {
-  return DraggableScrollableSheet(
-    initialChildSize: 0.2,
-    minChildSize: 0.2,
-    maxChildSize: 0.8,
-    builder: (BuildContext context, ScrollController scrollController) {
-      return Container(
-        decoration: BoxDecoration(
-          color: Color(0xfff9f9f9),
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(8),
-            topRight: Radius.circular(8),
-          ),
-        ),
-      );
-    },
-  );
 }
